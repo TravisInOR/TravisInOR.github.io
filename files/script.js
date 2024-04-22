f7685fc99783cc2e45418338c9e59c01062b1a35
TravisInOR <158083714+TravisInOR@users.noreply.github.com>
2024-04-22 12:43:02 -0700

    Update script.js

--- files/script.js
+++ files/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Programming" , "Cybersecurity" , "Web Development"],
+    strings: ["Programming" , "AI" , "Development"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
